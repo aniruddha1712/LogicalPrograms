f19ee01fea89c560b0d92bbde7aa234020dd3133
Aniruddha <aniruddha1712@gmail.com>
2022-03-31 13:28:58 +0530

    [Aniruddha]Added: Welcome message

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programs of C#");
+            Console.ReadLine();
+        }
+    }
+}
